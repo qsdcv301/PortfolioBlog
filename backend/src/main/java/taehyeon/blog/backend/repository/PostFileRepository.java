90a9964f8dba40ae764bed62ef187fe341cd38f7
qsdcv301 <130839376+qsdcv301@users.noreply.github.com>
2024-12-12 14:23:40 +0900

    start:PortfolioBlog

--- backend/src/main/java/taehyeon/blog/backend/repository/PostFileRepository.java
+++ backend/src/main/java/taehyeon/blog/backend/repository/PostFileRepository.java
@@ -3,5 +3,8 @@ package taehyeon.blog.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import taehyeon.blog.backend.entity.PostFile;
 
+import java.util.List;
+
 public interface PostFileRepository extends JpaRepository<PostFile, Long> {
+    List<PostFile> findAllByNTime(Long nTime);
 }
