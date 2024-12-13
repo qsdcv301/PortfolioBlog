171ede34cbff99ecfd991a9aeb5d05f67a877792
qsdcv301 <130839376+qsdcv301@users.noreply.github.com>
2024-12-13 17:40:55 +0900

    fix:Main 수정

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,14 +1,68 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import { boxShadow, padding16, borders } from "../style/style";
+import Banner from "../components/Banner";
 
-import Banner from '../components/Banner'
+import axios from "axios";
 
 const Main = () => {
-  return (
-    <Container>
-       <Banner />
-    </Container>
-  )
-}
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/api/posts")
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
-export default Main
+  //이미지 태그 제거 함수
+  const removeImgTags = (content) => {
+    return content.replace(/<img[^>]*>/g, "");
+  };
+
+  return (
+    <Container style={{ ...boxShadow, ...padding16, ...borders }}>
+      <Banner />
+      <Row className="mt-4">
+        {posts.map((post, index) =>
+          index === 0 ? (
+            <Col md="12" key={index}>
+              {post.firstImg && (
+                <Image
+                  src={`upload/images/${post.ntime}/${post.firstImg}`}
+                  fluid
+                  alt={post.title}
+                />
+              )}
+              <h3>{post.title}</h3>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: removeImgTags(post.content),
+                }}
+              />
+            </Col>
+          ) : (
+            <Col md="6" key={index}>
+              {post.firstImg && (
+                <Image
+                  src={`upload/images/${post.ntime}/${post.firstImg}`}
+                  fluid
+                  alt={post.title}
+                />
+              )}
+              <h3>{post.title}</h3>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: removeImgTags(post.content),
+                }}
+              />
+            </Col>
+          )
+        )}
+      </Row>
+    </Container>
+  );
+};
+
+export default Main;
