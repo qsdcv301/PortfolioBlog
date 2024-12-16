b2356918bb57d72a23185917e60dd6a27ae34015
qsdcv301 <130839376+qsdcv301@users.noreply.github.com>
2024-12-16 15:15:28 +0900

    fix:1차 수정

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,56 +1,88 @@
-import React, { useState, useEffect } from 'react'
-import { Container, Row, Col, Image } from 'react-bootstrap'
-import { boxShadow, padding16, borders } from '../style/style'
-import Banner from '../components/Banner'
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import { format } from "date-fns";
+import { boxShadow, padding16, borders } from "../style/style";
+import Banner from "../components/Banner";
 
-import axios from 'axios'
+import axios from "axios";
 
 const Main = () => {
   const [posts, setPosts] = useState([]);
-  useEffect(()=>{
-    axios.get('/api/posts')
-         .then(response => {
-            setPosts(response.data);
-         })
-         .catch(error=>console.error(error));
-  },[]);
- 
+  useEffect(() => {
+    axios
+      .get("/api/posts")
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
   //이미지 태그 제거 함수
   const removeImgTags = (content) => {
-     return content.replace(/<img[^>]*>/g, '');
+    return content.replace(/<img[^>]*>/g, "");
+  };
+
+  /*
+  yyyy 연도, MM월, dd 일, EEEE 요일, a(오전/오후)  h:m (시간,분)
+  */
+  const formatDate = (dt) => {
+    const date = new Date(dt);
+    return format(date, "yyyy년 MM월 dd일 a h:m");
   };
 
   return (
-    <Container style={{...boxShadow, ...padding16, ...borders}}>
-       <Banner />
-       <Row className="mt-4">
-        {
-            posts.map((post , index)=> (
-              (index === 0)?
-               <Col md="12" key={index}>
-                  {
-                    post.firstImg && (
-                       <Image src={`upload/images/${post.ntime}/${post.firstImg}`} fluid alt={post.title} />
-                    )
-                  }
-                  <h3>{post.title}</h3>
-                  <div dangerouslySetInnerHTML={{ __html: removeImgTags(post.content)}} />
-               </Col>
-              :
-               <Col md="6" key={index}>
-                  {
-                    post.firstImg && (
-                       <Image src={`upload/images/${post.ntime}/${post.firstImg}`} fluid alt={post.title} />
-                    )
-                  }
-                  <h3>{post.title}</h3>
-                  <div dangerouslySetInnerHTML={{ __html: removeImgTags(post.content)}} />
-               </Col> 
-            ))
-        }
-       </Row>
+    <Container style={{ ...boxShadow, ...padding16, ...borders }}>
+      <Banner />
+      <Row className="mt-4">
+        {posts.map((post, index) =>
+          index === 0 ? (
+            <Col md="12" key={index}>
+              {post.firstImg && (
+                <Image
+                  src={`upload/images/${post.ntime}/${post.firstImg}`}
+                  fluid
+                  alt={post.title}
+                  className="post-img"
+                />
+              )}
+              <div className="post-text-box">
+                <h3>{post.title}</h3>
+                <div className="post-event">
+                  <span className="bold mx-2"></span>
+                  <span className="bold text-danger mx-2"></span>
+                  <span className="ps-4">{post.createDate}</span>
+                </div>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: removeImgTags(post.content),
+                  }}
+                />
+              </div>
+            </Col>
+          ) : (
+            <Col md="6" key={index}>
+              {post.firstImg && (
+                <Image
+                  src={`upload/images/${post.ntime}/${post.firstImg}`}
+                  fluid
+                  alt={post.title}
+                  className="post-img2"
+                />
+              )}
+              <div className="post-text-box2">
+                <h3>{post.title}</h3>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: removeImgTags(post.content),
+                  }}
+                />
+              </div>
+            </Col>
+          )
+        )}
+      </Row>
     </Container>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
--- frontend/src/pages/RAside.jsx
+++ frontend/src/pages/RAside.jsx
@@ -1,56 +1,71 @@
-import React from 'react'
-import { Card, Row, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import { boxShadow, padding16 } from '../style/style'
-import { RiKakaoTalkFill,  RiInstagramFill , RiFacebookBoxFill, RiTwitterXFill } from "react-icons/ri";
+import React from "react";
+import { Card, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { boxShadow, padding16 } from "../style/style";
+import {
+  RiKakaoTalkFill,
+  RiInstagramFill,
+  RiFacebookBoxFill,
+  RiTwitterXFill,
+} from "react-icons/ri";
 
 const RAside = () => {
-
   const facebook = () => {
-      window.location.href="https://facebook.com";
-  }  
-    
+    window.location.href = "https://facebook.com";
+  };
+
   return (
     <>
-       <Card style={{ ...boxShadow, ...padding16 }}  className="mb-4">
-         <Card.Img variant="top" src="images/me.jpg" />
-         <Card.Body>
-            <Card.Title>Developer Andrea Lee</Card.Title>
-            <Card.Text>
-                Hello, I'm Andrea. A Content writer, Developer, and Teacher.
-                Working as a Content ...
-            </Card.Text>
-         </Card.Body>
-         <Row>
-            <Col md={{span:6, offset:6}}>
-                <Link to="about" className="btn btn-outline-success">자세히보기</Link>
-            </Col>
-         </Row>   
-       </Card>
+      <Card style={{ ...boxShadow, ...padding16 }} className="mb-4">
+        <Card.Img variant="top" src="images/me.jpg" />
+        <Card.Body>
+          <Card.Title>Developer Andrea Lee</Card.Title>
+          <Card.Text>
+            Hello, I'm Taehyeon. A Content writer, Developer. Working as a
+            Content ...
+          </Card.Text>
+        </Card.Body>
+        <Row>
+          <Col md={{ span: 6, offset: 6 }}>
+            <Link to="about" className="btn btn-outline-success">
+              자세히보기
+            </Link>
+          </Col>
+        </Row>
+      </Card>
 
-       <Card style={{ ...boxShadow, ...padding16 }} className="mb-4">
-          <Card.Title className="text-center">            
-            <RiKakaoTalkFill className="mx-2" />
-            <RiInstagramFill className="mx-2" />
-            <RiFacebookBoxFill className="mx-2" onClick={facebook} />
-            <RiTwitterXFill className="mx-2" />
-          </Card.Title>
-       </Card>
+      <Card style={{ ...boxShadow, ...padding16 }} className="mb-4">
+        <Card.Title className="text-center">
+          <RiKakaoTalkFill className="mx-2" />
+          <RiInstagramFill className="mx-2" />
+          <RiFacebookBoxFill className="mx-2" onClick={facebook} />
+          <RiTwitterXFill className="mx-2" />
+        </Card.Title>
+      </Card>
 
-       <Card style={{ ...boxShadow, ...padding16 }} className="mb-4">
-          <h4>Category</h4>
-          <ul className="list-style">
-             <li><Link to="post">posting</Link></li>
-             <li><Link to="about">about</Link></li>
-             <li><Link to="git">git</Link></li>
-             <li><Link to="contact">contact</Link></li>
-          </ul>
-       </Card>
+      <Card style={{ ...boxShadow, ...padding16 }} className="mb-4">
+        <h4>Category</h4>
+        <ul className="list-style">
+          <li>
+            <Link to="post">posting</Link>
+          </li>
+          <li>
+            <Link to="about">about</Link>
+          </li>
+          <li>
+            <Link to="git">git</Link>
+          </li>
+          <li>
+            <Link to="contact">contact</Link>
+          </li>
+        </ul>
+      </Card>
 
-       <Card style={{ ...boxShadow, ...padding16 }} className="mb-4">해시태그</Card>
-
+      <Card style={{ ...boxShadow, ...padding16 }} className="mb-4">
+        해시태그
+      </Card>
     </>
-  )
-}
+  );
+};
 
-export default RAside
+export default RAside;
