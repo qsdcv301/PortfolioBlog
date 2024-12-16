c8f0cf811ea4a94d4ca2d9f7f0389ebb54198a7a
qsdcv301 <130839376+qsdcv301@users.noreply.github.com>
2024-12-16 14:17:48 +0900

    fix:오탈자 및 데이터 수정 및 추가

--- frontend/src/components/git.js
+++ frontend/src/components/git.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-console.log("github token:" + GITHUB_TOKEN);
+if(!process.env.REACT_APP_GITHUB_TOKEN){
+    console.error("토큰을 읽을 수 없어요");
+}
+console.log(GITHUB_TOKEN);
 const api = axios.create({
-  baseURL: "https://api.github.com",
-  headers: {
-    Authorization: `Bearer ${GITHUB_TOKEN}`,
-    Accept: "application/vnd.github+json",
-  },
+    baseURL : 'https://api.github.com',
+    headers: {
+        Authorization: `Bearer ${GITHUB_TOKEN}`,
+        Accept : 'application/vnd.github+json'
+    }
 });
 
 export default api;
